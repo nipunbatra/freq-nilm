14eeeb57f99c4eed52eb52f966416bfb58f33659
Nipun Batra <nipunb@iiitd.ac.in>
2017-11-20 14:19:47 -0500

    correctd the path

--- code/calculate_error.py
+++ code/calculate_error.py
@@ -9,11 +9,11 @@ from common import hourly_4d
 
 freq = sys.argv[1]
 #freq = '2H'
-tensor = pickle.load(open('{}-input.pkl'.format(freq),'r'))
+tensor = pickle.load(open('../{}-input.pkl'.format(freq),'r'))
 error_rank = {}
 for r in range(1, 10):
 	print("*" * 20)
 
-	pred = pickle.load(open("{}-{}-pred-hourly.pkl".format(freq, r), 'r'))
+	pred = pickle.load(open("../{}-{}-pred-hourly.pkl".format(freq, r), 'r'))
 
 	error_rank[r] = hourly_4d(tensor, pred)
