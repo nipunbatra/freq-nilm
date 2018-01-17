01617160a02ffd8233d98c3d7f96889b19ed4d0f
ylwander <yj9xs@virginia.edu>
2018-01-17 12:01:42 -0500

    create baseline script

---
+++ code/baseline-mtf.py
@@ -0,0 +1,38 @@
+import sys
+import numpy as np
+import pandas as pd
+from dataloader import APPLIANCE_ORDER, get_train_test
+from tensor_custom_core import stf_4dim, stf_4dim_time
+num_folds = 5
+
+def mtf(r=2, lr=1, num_iter=100):
+    out = []
+    for cur_fold in range(5):
+        train, test = get_train_test(num_folds=num_folds, fold_num=cur_fold)
+        test_copy = test.copy()
+        test_copy[:, 1:, :, :] = np.NaN
+        train_test = np.concatenate([train, test_copy])
+        H, A, D, T = stf_4dim_time(tensor=train_test,r=r,lr=lr, num_iter=num_iter )
+        pred = np.einsum("Hr, Ar, Dr, ATr ->HADT", H, A, D, T)[len(train):, 1:, :, :]
+        out.append(pred)
+    return np.concatenate(out)
+
+
+tensor = np.load('../2015-5appliances.numpy.npy')
+from sklearn.metrics import mean_absolute_error
+
+err_mtf ={}
+gt = tensor[:, 1:, :, :]
+for num_latent in range(1, 2):
+	err_mtf[num_latent] = {}
+	for lr in [0.01, 0.1, 1, 2]:
+		err_mtf[num_latent][lr] = {}
+		for iters in range(100, 2500, 400):
+			print (num_latent, lr, iters)
+			pred = mtf(num_latent, lr, iters)
+			pred = np.minimum(pred, tensor[:, 0:1, :, :])
+			err_mtf[num_latent][lr][iters] = {APPLIANCE_ORDER[i+1]:mean_absolute_error(pred[:, i,:,:].flatten(), 
+                                                                       gt[:, i, :, :].flatten()) for i in range(pred.shape[1])}
+
+import pickle
+pickle.dump(err_mtf, open("../result/baseline-mtf.pkl", 'w'))
---
+++ code/baseline-sparse-coding-without-discriminative.py
@@ -0,0 +1,35 @@
+import sys
+import numpy as np
+import pandas as pd
+from dataloader import APPLIANCE_ORDER, get_train_test
+from ddsc import SparseCoding, reshape_for_sc
+from sklearn.metrics import mean_absolute_error
+num_folds = 5
+
+def non_discriminative(num_latent):
+    out = []
+    for cur_fold in range(5):
+        train, test = get_train_test(num_folds=num_folds, fold_num=cur_fold)
+        train_sc, test_sc = reshape_for_sc(train), reshape_for_sc(test)
+        train_data = np.array([train_sc[:, :, i ] for i in range(1, train.shape[1])]).swapaxes(1, 2)
+        c = SparseCoding()
+        c.train(train_data, num_latent=num_latent)
+        pred = c.disaggregate(test_sc[:, :, 0].swapaxes(0, 1)).swapaxes(0, 2).swapaxes(1, 2)
+        pred = pred.reshape(pred.shape[0], pred.shape[1], -1, 24)
+        out.append(pred)
+    return np.concatenate(out)
+
+tensor = np.load('../2015-5appliances.numpy.npy')
+
+err_non_disc ={}
+gt = tensor[:, 1:, :, :]
+for num_latent in range(1, 50):
+    print(num_latent)
+    pred = non_discriminative(num_latent)
+    # Clamping prediction to aggregate
+    pred = np.minimum(pred, tensor[:, 0:1, :,:])
+    err_non_disc[num_latent] = {APPLIANCE_ORDER[i+1]:mean_absolute_error(pred[:, i,:,:].flatten(), 
+                                                                       gt[:, i, :, :].flatten()) for i in range(pred.shape[1])}
+
+import pickle
+pickle.dump(err_non_disc, open("../result/baseline-sparse-coding-non-disc.pkl"), 'w')
