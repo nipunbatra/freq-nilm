c275ebdbefc46bc65543fe2f760eb265d1b4fbbc
ylwander <yj9xs@virginia.edu>
2018-02-05 20:14:56 -0500

    cluster script

--- code/cluster_script.py
+++ code/cluster_script.py
@@ -15,33 +15,34 @@ if not os.path.exists(SLURM_OUT):
 	os.makedirs(SLURM_OUT)
 
 # Max. num running processes you want. This is to prevent hogging the cluster
-MAX_NUM_MY_JOBS = 100
+MAX_NUM_MY_JOBS = 200
 # Delay between jobs when we exceed the max. number of jobs we want on the cluster
 DELAY_NUM_JOBS_EXCEEDED = 10
 import time
 
+for dataset in [1, 2]:
+	for cur_fold in range(5):
+		for num_latent in range(1, 21):
+			for lr in [0.01, 0.1, 1, 2]:
+				for iters in range(100, 2600, 400):
+					OFILE = "{}/{}-{}-{}-{}-{}.out".format(SLURM_OUT, dataset, cur_fold, num_latent, lr, iters)
+					EFILE = "{}/{}-{}-{}-{}-{}.err".format(SLURM_OUT, dataset, cur_fold, num_latent, lr, iters)
+					SLURM_SCRIPT = "{}/{}-{}-{}-{}-{}.pbs".format(SLURM_OUT, dataset, cur_fold, num_latent, lr, iters)
+					CMD = 'python3 baseline-stf-nested.py {} {} {} {} {}'.format(dataset, cur_fold, num_latent, lr, iters)
+					lines = []
+					lines.append("#!/bin/sh\n")
+					lines.append('#SBATCH --time=1-16:0:00\n')
+					lines.append('#SBATCH --mem=64\n')
+					#lines.append('#SBATCH -c 32\n')
+					lines.append('#SBATCH --exclude=artemis[1-5]\n')
+					lines.append('#SBATCH -o ' + '"' + OFILE + '"\n')
+					lines.append('#SBATCH -e ' + '"' + EFILE + '"\n')
+					lines.append(CMD + '\n')
+					with open(SLURM_SCRIPT, 'w') as f:
+						f.writelines(lines)
+					command = ['sbatch', SLURM_SCRIPT]
+					while len(delegator.run('squeue -u %s' % username).out.split("\n")) > MAX_NUM_MY_JOBS + 2:
+						time.sleep(DELAY_NUM_JOBS_EXCEEDED)
 
-for num_latent in range(1, 21):
-	for lr in [0.01, 0.1, 1, 2]:
-		for iters in range(100, 2500, 400):
-			OFILE = "{}/{}-{}-{}.out".format(SLURM_OUT, num_latent, lr, iters)
-			EFILE = "{}/{}-{}-{}.err".format(SLURM_OUT, num_latent, lr, iters)
-			SLURM_SCRIPT = "{}/{}-{}-{}.pbs".format(SLURM_OUT, num_latent, lr, iters)
-			CMD = 'python3 baseline-stf.py {} {} {}'.format(num_latent, lr, iters)
-			lines = []
-			lines.append("#!/bin/sh\n")
-			lines.append('#SBATCH --time=1-16:0:00\n')
-			lines.append('#SBATCH --mem=64\n')
-			#lines.append('#SBATCH -c 32\n')
-			lines.append('#SBATCH --exclude=artemis[1-5]\n')
-			lines.append('#SBATCH -o ' + '"' + OFILE + '"\n')
-			lines.append('#SBATCH -e ' + '"' + EFILE + '"\n')
-			lines.append(CMD + '\n')
-			with open(SLURM_SCRIPT, 'w') as f:
-				f.writelines(lines)
-			command = ['sbatch', SLURM_SCRIPT]
-			while len(delegator.run('squeue -u %s' % username).out.split("\n")) > MAX_NUM_MY_JOBS + 2:
-				time.sleep(DELAY_NUM_JOBS_EXCEEDED)
-
-			delegator.run(command, block=False)
-			print (SLURM_SCRIPT)
+					delegator.run(command, block=False)
+					print (SLURM_SCRIPT)
