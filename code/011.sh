26bbdd518bfc6abc5a89c97981bcbcc17cd28971
ylwander <yj9xs@virginia.edu>
2018-02-07 21:32:55 -0500

    add scripts

--- code/011.sh
+++ code/011.sh
@@ -1,10 +1,10 @@
 #!bin/bash
 
 
-appliance='hvac'
-fold=1
+appliance='hvac dr'
+fold=0
 
-for dataset in 1 2
+for dataset in 1
 do
 	for cell_type in 'GRU' 'LSTM' 'RNN'
 	do
@@ -12,13 +12,13 @@ do
 		do
 			for num_layers in 1 2 3 4
 			do
-				for bidirectional in 'True' 'False'
+				for bidirectional in 'True'
 				do
 					for lr in 0.01 0.1 1
 					do
-						for iterations in 1000 2000 3000
+						for iterations in 3000
 						do
-							CUDA_VISIBLE_DEVICES=1 python rnn-nested-cv.py $fold $dataset $cell_type $hidden_size $num_layers $bidirectional $lr $iterations 0 $appliance
+							CUDA_VISIBLE_DEVICES=1 python rnn-nested-cv-new.py $fold $dataset $cell_type $hidden_size $num_layers $bidirectional $lr $iterations 0 $appliance
 						done
 					done
 				done
