829014d15efdaa235a6e5025c4c7124d5e1e3d0a
ylwander <yj9xs@virginia.edu>
2018-02-10 13:30:46 -0500

    script

--- code/gpu020.sh
+++ code/gpu020.sh
@@ -1,33 +1,42 @@
 #!bin/bash
 
+CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 hvac fridge dr dw mw
+CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 hvac fridge dr mw dw
+CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 hvac fridge dw dr mw
+CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 hvac fridge dw mw dr
+CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 hvac fridge mw dr dw
+CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 hvac fridge mw dw dr
+CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 hvac dr fridge dw mw
+CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 hvac dr fridge mw dw
+CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 hvac dr dw fridge mw
+CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 hvac dr dw mw fridge
+CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 hvac dr mw fridge dw
+CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 hvac dr mw dw fridge
+CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 hvac dw fridge dr mw
+CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 hvac dw fridge mw dr
+CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 hvac dw dr fridge mw
+CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 hvac dw dr mw fridge
+CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 hvac dw mw fridge dr
+CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 hvac dw mw dr fridge
+CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 hvac mw fridge dr dw
+CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 hvac mw fridge dw dr
+CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 hvac mw dr fridge dw
+CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 hvac mw dr dw fridge
+CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 hvac mw dw fridge dr
+CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 hvac mw dw dr fridge
+CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 fridge hvac dr dw mw
 
-CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 dr hvac dw fridge mw
-CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 dr hvac dw mw fridge
-CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 dr hvac mw fridge dw
-CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 dr hvac mw dw fridge
-CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 dr fridge hvac dw mw
-CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 dr fridge hvac mw dw
-CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 dr fridge dw hvac mw
-CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 dr fridge dw mw hvac
-CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 dr fridge mw hvac dw
-CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 dr fridge mw dw hvac
-CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 dr dw hvac fridge mw
-CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 dr dw hvac mw fridge
-CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 dr dw fridge hvac mw
-CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 dr dw fridge mw hvac
-CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 dr dw mw hvac fridge
-CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 dr dw mw fridge hvac
-CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 dr mw hvac fridge dw
-CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 dr mw hvac dw fridge
-CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 dr mw fridge hvac dw
-CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 dr mw fridge dw hvac
-CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 dr mw dw hvac fridge
-CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 dr mw dw fridge hvac
-CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 dw hvac fridge dr mw
-CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 dw hvac fridge mw dr
 
-# appliance='fridge hvac dr mw dw'
-# for fold in 1
+
+
+# p=0.2
+# for random_seed in {1..10}
+# do
+#     CUDA_VISIBLE_DEVICES=0 python rnn-tree-p.py $p $random_seed
+# done
+
+# appliance='fridge hvac dr dw mw'
+# for fold in 0
 # do
 #     for dataset in 1
 #     do
