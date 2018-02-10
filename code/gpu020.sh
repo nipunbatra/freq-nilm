ec7f180aede7d58c7845c5fd8416e49d7c9621d0
ylwander <yj9xs@virginia.edu>
2018-02-10 15:05:30 -0500

    script

--- code/gpu020.sh
+++ code/gpu020.sh
@@ -1,63 +1,4 @@
 #!bin/bash
-<<<<<<< HEAD
-<<COMMENT1
-appliance='fridge hvac dr dw mw'
-for fold in 2
-do
-    for dataset in 1
-    do
-        for lr in 0.01 0.1 1
-        do
-            for iters in 1000 2000 3000
-            do
-                echo $appliance $fold $dataset $lr $iters
-                CUDA_VISIBLE_DEVICES=0 python dnn-nested-cv.py $fold $dataset $lr $iters 0 $appliance
-            done
-        done
-    done
-done
-COMMENT1
-
-
-appliance='fridge hvac dw'
- #fold=0
-for fold in 3
-do
-    for dataset in 2
-    do
-        for cell_type in 'LSTM' 'RNN'
-        do
-            for hidden_size in 20 50 100
-            do
-                for num_layers in 1 2 3
-                do
-                    if [ $hidden_size -eq 100 -a $num_layers -eq 2 ]
-                    then
-                        continue
-                    fi
-
-                    if [ $hidden_size -eq 100 -a $num_layers -eq 3 ]
-                    then
-                        continue
-                    fi
-
-                    for bidirectional in 'True'
-                    do
-                        for lr in 0.01 0.1 1
-                        do
-                            for iterations in 3000
-                            do
-                                echo $fold $dataset $cell_type $hidden_size $num_layers $bidirectional $lr $iterations 0 $appliance
-                                CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv-new.py $fold $dataset $cell_type $hidden_size $num_layers $bidirectional $lr $iterations 0 $appliance
-                            done
-                        done
-                    done
-                done
-            done
-        done
-    done
-done
-=======
 
 CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 hvac fridge dr dw mw
 CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 hvac fridge dr mw dw
@@ -168,4 +109,3 @@ CUDA_VISIBLE_DEVICES=0 python rnn-nested-cv.py 4 1 GRU 50 1 True 0.1 2000 0 frid
 #         done
 #     done
 # done
->>>>>>> 829014d15efdaa235a6e5025c4c7124d5e1e3d0a
