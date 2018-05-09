c5d6f75cfb59c57c1d15cfcfecf9b46a6558df2e
ylwander <yj9xs@virginia.edu>
2018-05-09 12:17:21 -0400

    add test resutls

--- code/calculate_error_new_metric_per_home.py
+++ code/calculate_error_new_metric_per_home.py
@@ -263,19 +263,124 @@ for dataset in [1, 3]:
     cnn_ind_errors[dataset], cnn_ind_best_error[dataset], cnn_ind_best_param[dataset] = calculate_cnn_valid_error(dataset, threshold)
     print("calculate cnn tree on dataset {}".format(dataset))
     cnn_tree_errors[dataset], cnn_tree_best_error[dataset], cnn_tree_best_param[dataset] = calculate_cnn_tree_valid_error(dataset, threshold)
+
+# for stf
+stf_pred = {}
+for dataset in [1, 3]:
+    stf_pred[dataset] = {}
+    for fold_num in range(5):
+        stf_pred[dataset][fold_num] = {}
+            
+        num_latent = stf_best_param[dataset][fold_num]['num_latent']
+        lr = float(stf_best_param[dataset][fold_num]['lr'])
+        iters = stf_best_param[dataset][fold_num]['iters']
+
+        result = np.load("../code/baseline/stf/{}/test/stf-test-pred-{}-{}-{}-{}-{}.npy".format(dataset, dataset, fold_num, num_latent, lr, iters))
+        for idx, appliance in enumerate(APPLIANCE_ORDER[1:-1]): 
+            stf_pred[dataset][fold_num][appliance] = result[:, idx]
+
+stf_best_test = {}
+stf_best_overall = {}
+for dataset in [1, 3]:
+    stf_best_test[dataset], stf_best_overall[dataset] = calculate_error(stf_pred[dataset], test_gt, threshold)
+
+# for mtf
+mtf_pred = {}
+for dataset in [1, 3]:
+    mtf_pred[dataset] = {}
+    for fold_num in range(5):
+        mtf_pred[dataset][fold_num] = {}
+            
+        num_latent = mtf_best_param[dataset][fold_num]['num_latent']
+        lr = float(mtf_best_param[dataset][fold_num]['lr'])
+        iters = mtf_best_param[dataset][fold_num]['iters']
+
+        result = np.load("../code/baseline/mtf/{}/test/mtf-test-pred-{}-{}-{}-{}-{}.npy".format(dataset, dataset, fold_num, num_latent, lr, iters))
+        for idx, appliance in enumerate(APPLIANCE_ORDER[1:-1]): 
+            mtf_pred[dataset][fold_num][appliance] = result[:, idx]
+
+mtf_best_test = {}
+mtf_best_overall = {}
+for dataset in [1, 3]:
+    mtf_best_test[dataset], mtf_best_overall[dataset] = calculate_error(mtf_pred[dataset], test_gt, threshold)
+
+
+# for cnn individual
+# get prediction for cnn individual
+cnn_individual_pred = {}
+for dataset in [1, 3]:
+    cnn_individual_pred[dataset] = {}
+    for fold_num in range(5):
+        cnn_individual_pred[dataset][fold_num] = {}
+        for appliance in ['hvac', 'fridge', 'dr', 'dw', 'mw']:
+            
+            lr = cnn_ind_best_param[dataset][fold_num][appliance]['lr']
+            iters = cnn_ind_best_param[dataset][fold_num][appliance]['iters']
+
+            directory = "../code/baseline/cnn-tree/{}/{}/{}/200000/0.0/".format(dataset, fold_num,lr)
+            filename = "test-pred-[\'{}\'].npy".format(appliance)
+
+            full_path = directory + filename
+            cnn_individual_pred[dataset][fold_num][appliance] = np.asarray(np.load(full_path).item()[iters]).reshape(-1, 1, 112, 24)
+# get prediction for cnn tree
+cnn_tree_pred = {}
+for dataset in [1, 3]:
+    cnn_tree_pred[dataset] = {}
+    for fold_num in range(5):
+        cnn_tree_pred[dataset][fold_num] = {}
+            
+        lr = cnn_tree_best_param[dataset][fold_num]['lr']
+        iters = cnn_tree_best_param[dataset][fold_num]['iters']
+        order = cnn_tree_best_param[dataset][fold_num]['order']
+
+        o = "\', \'".join(str(x) for x in order)
+        directory = "../code/baseline/cnn-tree/{}/{}/{}/20000/0.0/".format(dataset, fold_num,lr)
+        filename = "test-pred-[\'{}\'].npy".format(o)
+
+        full_path = directory + filename
+        cnn_pred = np.asarray(np.load(full_path).item()[iters])
+        
+        for idx, appliance in enumerate(order):
+            cnn_tree_pred[dataset][fold_num][appliance] = cnn_pred[idx].reshape(-1, 1, 112,24)
+    
+
+cnn_ind_best_test = {}
+cnn_ind_best_overall = {}
+cnn_tree_best_test = {}
+cnn_tree_best_overall = {}
+for datset in [1, 3]:
+    cnn_ind_best_test[dataset], cnn_ind_best_overall[dataset] = calculate_error(cnn_individual_pred[dataset], test_gt, threshold)
+    cnn_tree_best_test[dataset], cnn_tree_best_overall[dataset] = calculate_error(cnn_tree_pred[dataset], test_gt, threshold)
+
+
+
+
+
 print("save results")
-np.save("{}/stf_errors.npy".format(directory), stf_errors)
-np.save("{}/stf_best_error.npy".format(directory), stf_best_error)
+np.save("{}/stf_valid_errors.npy".format(directory), stf_errors)
+np.save("{}/stf_best_valid_error.npy".format(directory), stf_best_error)
 np.save("{}/stf_best_param.npy".format(directory), stf_best_param)
-np.save("{}/mtf_errors.npy".format(directory), mtf_errors)
-np.save("{}/mtf_best_error.npy".format(directory), mtf_best_error)
+np.save("{}/stf_best_test.npy".format(directory), stf_best_test)
+np.save("{}/stf_best_overall.npy".format(directory), stf_best_overall)
+
+np.save("{}/mtf_valid_errors.npy".format(directory), mtf_errors)
+np.save("{}/mtf_best_valid_error.npy".format(directory), mtf_best_error)
 np.save("{}/mtf_best_param.npy".format(directory), mtf_best_param)
-np.save("{}/cnn_ind_errors.npy".format(directory), cnn_ind_errors)
-np.save("{}/cnn_ind_best_error.npy".format(directory), cnn_ind_best_error)
+np.save("{}/mtf_best_test.npy".format(directory), mtf_best_test)
+np.save("{}/mtf_best_overall.npy".format(directory), mtf_best_overall)
+
+np.save("{}/cnn_ind_valid_errors.npy".format(directory), cnn_ind_errors)
+np.save("{}/cnn_ind_best_valid_error.npy".format(directory), cnn_ind_best_error)
 np.save("{}/cnn_ind_best_param.npy".format(directory), cnn_ind_best_param)
-np.save("{}/cnn_tree_errors.npy".format(directory), cnn_tree_errors)
-np.save("{}/cnn_tree_best_error.npy".format(directory), cnn_tree_best_error)
+np.save("{}/cnn_ind_best_test.npy".format(directory), cnn_ind_best_test)
+np.save("{}/cnn_ind_best_overall.npy".format(directory), cnn_ind_best_overall)
+
+np.save("{}/cnn_tree_valid_errors.npy".format(directory), cnn_tree_errors)
+np.save("{}/cnn_tree_best_valid_error.npy".format(directory), cnn_tree_best_error)
 np.save("{}/cnn_tree_best_param.npy".format(directory), cnn_tree_best_param)
+np.save("{}/cnn_tree_best_test.npy".format(directory), cnn_tree_best_test)
+np.save("{}/cnn_tree_best_overall.npy".format(directory), cnn_tree_best_overall)
+
 
 
 
