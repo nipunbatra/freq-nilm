91d9709b527141c45a5cbdb3eb8f38fb188dcce4
ylwander <yj9xs@virginia.edu>
2018-08-21 16:57:22 -0400

    modify the name

--- code/011.sh
+++
@@ -1,7 +0,0 @@
-#!bin/bash
-CUDA_VISIBLE_DEVIECS=1 python cnn-tree-aug.py 3 0.01 20000 0.0 3 0.2 2 fridge hvac dr mw dw 
-CUDA_VISIBLE_DEVIECS=1 python cnn-tree-aug.py 3 0.01 20000 0.0 3 1.0 4 fridge hvac dr mw dw 
-CUDA_VISIBLE_DEVIECS=1 python cnn-tree-aug.py 3 0.01 20000 0.0 3 2.0 3 fridge hvac dr mw dw 
-CUDA_VISIBLE_DEVIECS=1 python cnn-tree-aug.py 3 0.01 20000 0.0 3 5.0 0 fridge hvac dr mw dw 
-CUDA_VISIBLE_DEVIECS=1 python cnn-tree-aug.py 3 0.01 20000 0.0 3 5.0 3 fridge hvac dr mw dw 
-CUDA_VISIBLE_DEVIECS=1 python cnn-tree-aug.py 3 0.1 20000 0.0 4 0.2 0 dr hvac mw dw fridge 
--- code/012.sh
+++
@@ -1,7 +0,0 @@
-#!bin/bash
-CUDA_VISIBLE_DEVIECS=1 python cnn-tree-aug.py 3 0.1 20000 0.0 4 0.2 1 dr hvac mw dw fridge 
-CUDA_VISIBLE_DEVIECS=1 python cnn-tree-aug.py 3 0.1 20000 0.0 4 0.2 3 dr hvac mw dw fridge 
-CUDA_VISIBLE_DEVIECS=1 python cnn-tree-aug.py 3 0.1 20000 0.0 4 0.2 4 dr hvac mw dw fridge 
-CUDA_VISIBLE_DEVIECS=1 python cnn-tree-aug.py 3 0.1 20000 0.0 4 0.5 0 dr hvac mw dw fridge 
-CUDA_VISIBLE_DEVIECS=1 python cnn-tree-aug.py 3 0.1 20000 0.0 4 0.5 1 dr hvac mw dw fridge 
-CUDA_VISIBLE_DEVIECS=1 python cnn-tree-aug.py 3 0.1 20000 0.0 4 0.5 2 dr hvac mw dw fridge 
